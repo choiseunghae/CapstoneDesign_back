9682eb584b586d9bcf78b5767b13110222a1f8ac
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-03-23 17:35:39 +0900

    npm

--- mysql.js
+++ mysql.js
@@ -16,9 +16,32 @@ connection.query('SELECT * FROM detailpage', function (error, results, fields) {
   console.log(results);
 });
 
+
 module.exports = connection;
 
 
+/* const mysql = require('promise-mysql');
+const fs = require('fs');
+
+// createTcpPool initializes a TCP connection pool for a Cloud SQL
+// instance of MySQL.
+const createTcpPool = async config => {
+  // Note: Saving credentials in environment variables is convenient, but not
+  // secure - consider a more secure solution such as
+  // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+  // keep secrets safe.
+  const dbConfig = {
+    host: process.env.INSTANCE_HOST, // e.g. '127.0.0.1'
+    port: process.env.DB_PORT, // e.g. '3306'
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    // ... Specify additional properties here.
+    ...config,
+  };
+  // Establish a connection to the database.
+  return mysql.createPool(dbConfig);
+};  */
 /* server.get('/dictionary', (req, res) => {
   connection.query('SELECT * FROM detailpage', (err, rows) => {
     if (err) throw err;
