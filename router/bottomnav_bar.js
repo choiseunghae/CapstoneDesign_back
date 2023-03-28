ad5811ccf547b30454c65c6ef719203f6a0118ed
imgracehwang <hwangpadark@gmail.com>
2023-03-28 12:51:58 +0900

    commit

--- router/bottomnav_bar.js
+++ router/bottomnav_bar.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.use((req, res, next) => {
-    let html = `<nav class="nav_form">
-      <ul class="nav__list">
-        <li class="nav__btn"><a class="nav__link" href="/"><i class="bi bi-bookmark-fill"></i></a></li>
-        <li class="nav__btn"><a class="nav__link" href="#"><i class="bi bi-chat-dots-fill"></i></a></li>
-        <li class="nav__btn"><a class="nav__link" href="/mainpage"><i class="bi bi-house-door-fill"></i></a></li>
-        <li class="nav__btn"><a class="nav__link" href="/dictionary"><i class="bi bi-search"></i></a></li>
-        <li class="nav__btn"><a class="nav__link" href="/"><i class="bi bi-person-fill"></i></a></li>
-      </ul>
-      </nav>`;
+    let html = `
+    <ul class="nav__form">
+                <li class="nav__list"><a href="bookmark"><i class="bi bi-bookmark-fill" style="font-size: 2rem; color: black;"></i></a></li>
+                <li class="nav__list"><a href="#"><i class="bi bi-chat-dots-fill" style="font-size: 2rem; color: black;"></i></a></li>
+                <li class="nav__list"><a href="mainpage"><i class="bi bi-house-door-fill" style="font-size: 2rem; color: black;"></i></a></li>
+                <li class="nav__list"><a href="word"><i class="bi bi-search" style="font-size: 2rem; color: black;"></i></a></li>
+                <li class="nav__list"><a href="mypage"><i class="bi bi-person-fill" style="font-size: 2rem; color: black;"></i></a></li>
+            </ul>
+            
+            `;
   
     res.locals.bottom = html;
     next();
