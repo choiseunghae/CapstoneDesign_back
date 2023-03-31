cc40adf236cb26cb547ac203bbdb4b89e796d5a8
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-03-31 15:06:35 +0900

    bookmark

--- router/bookmark.js
+++ router/bookmark.js
@@ -3,25 +3,30 @@ const router = express.Router();
 const connection = require('../mysql');
 const headerRouter = require('./headernav_bar');
 const navitemRouter = require('./navitem_bar');
-const bottomRouter = require('./bottomnav_bar'); 
-
+const bottomRouter = require('./bottomnav_bar');
 
 router.use(headerRouter);
-router.use(bottomRouter); 
-router.use(navitemRouter); 
+router.use(bottomRouter);
+router.use(navitemRouter);
 
 router.get('/', (req, res) => {
-    let html=`<div class="my-2 p-3">      
+  connection.query('SELECT * FROM detailpage', (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.send('Error occurred');
+      return;
+    }
+    let html = `<div class="my-2 p-3">      
     <div class="bookmark d-flex justify-content-between">
       <div class="bookmark__word">북마크 단어</div>
      </div>
     `;
 
-    
+
     for (let i = 0; i < rows.length; i++) {
       const row = rows[i];
 
-      html +=`<ul class="list-group border-2">
+      html += `<ul class="list-group border-2">
       <li class="list-group-item d-flex" style="border-color:#D0E450"><a href="word">
       ${row.itemName}</a>
         <div><i class="bi bi-bookmark-fill" onclick="toggleIcon(this)"></i></div>
@@ -30,9 +35,10 @@ router.get('/', (req, res) => {
     }
 
     res.render('bookmark', { bookmark: html });
-
   });
-  
+
+});
+
 
 module.exports = router;
 
