3045db080023f4ca8967b422622d896a99544629
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-03-31 15:11:30 +0900

    bookmark2

--- router/bookmark.js
+++ router/bookmark.js
@@ -27,8 +27,7 @@ router.get('/', (req, res) => {
       const row = rows[i];
 
       html += `<ul class="list-group border-2">
-      <li class="list-group-item d-flex" style="border-color:#D0E450"><a href="word">
-      ${row.itemName}</a>
+      <li class="list-group-item d-flex" style="border-color:#D0E450"><a href="detail/${row.itemIndex}">${row.itemName}</a>
         <div><i class="bi bi-bookmark-fill" onclick="toggleIcon(this)"></i></div>
       </li>
       `;
