232cf81a06e49f4296bce558f091cfe704aa1667
byeonjieun <bje6365@gmail.com>
2023-04-07 18:52:40 +0900

    list

--- router/bookmark.js
+++ router/bookmark.js
@@ -19,13 +19,14 @@ router.get('/', (req, res) => {
 
 
     for (let i = 0; i < rows.length; i++) {
+
       const row = rows[i];
 
       html += `<ul class="list-group border-2">
-      <li class="list-group-item d-flex" style="border-color:#D0E450"><a href="detail/${row.itemIndex}">${row.itemName}</a>
+      <li class="list-group-item d-flex" style="border-color:#D0E450""><a href="detail/${row.itemIndex}">${row.itemName}</a>
         <div><i class="bi bi-bookmark-fill" onclick="toggleIcon(this)"></i></div>
       </li>
-      </ul>
+      </ul>  
       `;
     }
 
@@ -36,19 +37,5 @@ router.get('/', (req, res) => {
 
 
 module.exports = router;
-
-/*
-function toggleIcon(icon) {
-  if (icon.classList.contains('bi-bookmark-fill')) {
-    // 북마크가 설정되었을 경우 해제하기
-    icon.classList.remove('bi-bookmark-fill');
-    icon.classList.add('bi-bookmark');
-  } else {
-    // 북마크가 해제되었을 경우 설정하기
-    icon.classList.remove('bi-bookmark');
-    icon.classList.add('bi-bookmark-fill');
-  }
-}
-*/
 
 
