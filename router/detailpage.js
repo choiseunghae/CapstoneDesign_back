97aa5625ab64aae8cfa7e9e811572405b6e42273
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-04-14 13:14:10 +0900

    word

--- router/detailpage.js
+++ router/detailpage.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../mysql'); // mysql 모듈을 가져옴
-const searchboxRouter = require('./search_box');
 const bottomRouter = require('./bottomnav_bar'); // bottomnav_bar.js 파일을 불러옵니다.
 
-router.use(searchboxRouter);
 router.use(bottomRouter); // bottom 미들웨어를 사용합니다.
 
 router.get('/:id', (req, res) => {
@@ -17,9 +15,66 @@ router.get('/:id', (req, res) => {
     }
     const { itemName, itemDescription, itemDescription2 } = rows[0];
 
-    // HTML 응답 보내기
-    res.render('word', { itemName, itemDescription, itemDescription2 });
+    // 북마크 정보 가져오기
+    const userId = req.session.userIndex;
+    if (userId) {
+      connection.query(`SELECT * FROM mybookmarkpage WHERE itemIndex = '${id}' AND userIndex = '${userId}'`, (err, rows) => {
+        if (err) {
+          console.log(err);
+          res.send('Error occurred');
+          return;
+        }
+        const isBookmarked = rows.length > 0;
+        // HTML 응답 보내기
+        res.render('word', { itemName, itemDescription, itemDescription2, isBookmarked });
+      });
+    } else {
+      // HTML 응답 보내기
+      res.render('word', { itemName, itemDescription, itemDescription2 });
+    }
   });
 });
+
+router.post('/:id', (req, res) => {
+  const id = req.params.id;
+  const userId = req.session.userIndex;
+  
+  // 로그인되어 있지 않은 경우
+  if (!userId) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+  
+  connection.query(`SELECT * FROM mybookmarkpage WHERE itemIndex = '${id}' AND userIndex = '${userId}'`, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error occurred');
+      return;
+    }
+
+    if (rows.length > 0) {
+      // 북마크가 이미 설정되어 있으면 해제
+      connection.query(`DELETE FROM mybookmarkpage WHERE itemIndex = '${id}' AND userIndex = '${userId}'`, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error occurred');
+          return;
+        }
+        res.json({ bookmarked: false });
+      });
+    } else {
+      // 북마크가 설정되어 있지 않으면 설정
+      connection.query(`INSERT INTO mybookmarkpage (itemIndex, userIndex) VALUES ('${id}', '${userId}')`, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error occurred');
+          return;
+        }
+        res.json({ bookmarked: true });
+      })
+    }
+  })
+});
+
 
 module.exports = router;
--- router/login.js
+++ router/login.js
@@ -10,7 +10,7 @@ router.use(cookieParser());
 router.get('/', (req, res) => {
     if (req.session.usernickname) {
         return res.redirect('/mypage');
-      }
+    }
     res.render('login');
 });
 
@@ -22,25 +22,30 @@ router.post('/', (req, res) => {
         if (error) throw error;
         if (result.length === 1) {
             // 로그인 성공
+            const userIndex = result[0].userIndex; // userIndex 추출
             const authToken = generateAuthToken(); // 인증 토큰 생성
+
             function generateAuthToken() {
                 let authToken = '';
                 const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    
+
                 // 랜덤한 문자열을 32자 생성
                 for (let i = 0; i < 32; i++) {
                     authToken += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
                 }
-    
+
                 return authToken;
             }
+
             res.cookie('authToken', authToken, { maxAge: 86400000, httpOnly: true }); // 쿠키에 인증 토큰 저장
-            req.session.usernickname = usernickname; // 세션에 유저 정보 저장
+            req.session.userIndex = userIndex; // 세션에 userIndex 저장
+            req.session.usernickname = usernickname; // 세션에 usernickname 저장
             res.redirect("/mainpage");
         } else {
             res.render('login', { message: '회원 정보가 다릅니다.' });
         }
     });
 });
+
 
 module.exports = router;
