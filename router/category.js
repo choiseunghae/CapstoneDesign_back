3f94ee796ccfe92210177f9c6d3a14ec41a98d64
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-04-20 16:29:41 +0900

    category

--- router/category.js
+++ router/category.js
@@ -54,52 +54,55 @@ router.get('/', (req, res) => {
     });
 });
 
-
 router.get('/:categoryId', (req, res) => {
     const categoryId = req.params.categoryId;
-    connection.query('SELECT * FROM detailpage', (err, rows) => {
-        if (err) {
-            console.log(err);
-            res.send('Error occurred');
-            return;
-        }
-        connection.query(`SELECT * FROM detailpage WHERE categoryId = ${categoryId}`, (err, rows) => {
-            if (err) {
-                console.log(err);
-                res.send('Error occurred');
-                return;
-            }
-
-            let html = `
-            <div class="category_all">
-            <ol id="entryList" class="list_all">
-        `;
-
-            let tier = 1;
-
-            for (let i = 0; i < 9 && i < rows.length; i++) {
-                const row = rows[i];
-                const description = row.itemDescription.length > 30 ? row.itemDescription.substring(0, 30) + "..." : row.itemDescription;
-                html += `
-                <li class="best_type${tier}">
-                    <div class="list_contents">
-                        <div class="langking01">BEST ${tier}</div>
-                        <div class="list_content_name"><a href="/detail/${row.itemIndex}">${row.itemName}</a></div>
-                        <div class="list_content_content">${description}</div>
-                    </div>
-                </li>
-            `;
-                tier++;
-            }
-
-            html += `
-            </ol>
+  
+    connection.query(`
+      SELECT detailpage.*, COUNT(mythumbspage.itemIndex) AS thumbsCount
+      FROM detailpage
+      LEFT JOIN mythumbspage ON detailpage.itemIndex = mythumbspage.itemIndex AND mythumbspage.thumbsBool = 1
+      WHERE detailpage.categoryId = ?
+      GROUP BY detailpage.itemIndex
+      ORDER BY thumbsCount DESC
+      LIMIT 9
+    `, [categoryId], (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.send('Error occurred');
+        return;
+      }
+  
+      let html = `
+        <div class="category_all">
+          <ol id="entryList" class="list_all">
+      `;
+  
+      let tier = 1;
+  
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const description = row.itemDescription.length > 30 ? row.itemDescription.substring(0, 30) + "..." : row.itemDescription;
+        html += `
+          <li class="best_type${tier}">
+            <div class="list_contents">
+              <div class="langking${tier}">BEST ${tier}</div>
+              <div class="list_content_name${tier}"><a href="/detail/${row.itemIndex}">${row.itemName}</a></div>
+              <div class="list_content_content${tier}">${description}</div>
+              <div class="list_content_likes${tier}">${row.thumbsCount} Likes</div>
             </div>
+          </li>
         `;
-
-            res.render('category', { category: html });
-        });
+        tier++;
+      }
+  
+      html += `
+          </ol>
+        </div>
+      `;
+  
+      res.render('category', { category: html });
     });
-});
+  });
+  
 
 module.exports = router;
