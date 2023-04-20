111a618fd2f5eba416e340e6792ef933d3c46cf7
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-04-20 17:49:53 +0900

    search

--- router/search.js
+++ router/search.js
@@ -19,26 +19,61 @@ router.get('/', (req, res) => {
 router.get('/search', (req, res) => {
   const searchWord = req.query.searchWord;
 
-  connection.query(`SELECT * FROM detailpage WHERE itemName LIKE '%${searchWord}%' OR itemDescription LIKE '%${searchWord}%' OR itemDescription2 LIKE '%${searchWord}%'`, (error, results) => {
-    if (error) {
-      console.error(error);
-      return res.status(500).send('Internal Server Error');
+  connection.query(`SELECT *,
+    CASE 
+      WHEN itemName LIKE '%${searchWord}%' THEN 1 
+      ELSE 0 
+    END AS nameMatch,
+    CASE 
+      WHEN itemDescription LIKE '%${searchWord}%' THEN 1 
+      ELSE 0 
+    END AS descriptionMatch,
+    CASE 
+      WHEN itemDescription2 LIKE '%${searchWord}%' THEN 1 
+      ELSE 0 
+    END AS description2Match
+    FROM detailpage 
+    WHERE itemName LIKE '%${searchWord}%' OR itemDescription LIKE '%${searchWord}%' OR itemDescription2 LIKE '%${searchWord}%' 
+    ORDER BY (nameMatch + descriptionMatch + description2Match) DESC`, 
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+      }
+
+      if (results.length === 0) {
+        return res.render('search', { searchWord, items: null, message: '검색 결과를 찾을 수 없습니다.' });
+      }
+
+      // 검색 결과를 연관성 순으로 그룹화
+      const groups = {
+        단어명: [],
+        뜻: [],
+        예시: []
+      };
+
+      results.forEach(result => {
+        const { itemName, itemDescription, itemDescription2 } = result;
+        const matchScores = [result.nameMatch, result.descriptionMatch, result.description2Match];
+        const maxScore = Math.max(...matchScores);
+        if (maxScore === 1) {
+          if (result.nameMatch === 1) {
+            groups.단어명.push({ itemName, itemDescription });
+          } else if (result.descriptionMatch === 1) {
+            groups.뜻.push({ itemName, itemDescription });
+          } else if (result.description2Match === 1) {
+            groups.예시.push({ itemName, itemDescription });
+          }
+        }
+      });
+
+      // 검색어 리스트를 검색 페이지에 표시
+      res.render('search', { searchWord, groups });
     }
-
-    // 검색 결과가 없는 경우
-    if (results.length === 0) {
-      return res.status(404).send('검색 결과를 찾을 수 없습니다.');
-    }
-
-    const items = results.map(result => {
-      const { itemIndex, itemName, itemDescription } = result;
-      return { itemIndex, itemName, itemDescription };
-    });
-
-    // 검색어 리스트를 검색 페이지에 표시
-    res.render('search', { searchWord, items });
-  });
+  );
 });
+
+
 
 
 
