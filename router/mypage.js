c0dc0c41b2908262f79da1e72943413f598ae4cb
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-04-25 16:00:57 +0900

    mypage

--- router/mypage.js
+++ router/mypage.js
@@ -33,52 +33,52 @@ router.get('/', (req, res) => {
       }
       const thumbsCount = thumbsResult[0].thumbsCount;
 
-      // users 테이블에서 해당 유저의 프로필 이미지 blob 데이터 가져오기
+      // users 테이블에서 해당 유저의 프로필 이미지 경로 가져오기
       connection.query(`SELECT userImage FROM users WHERE userIndex = ${userId}`, (err, imageResult) => {
         if (err) {
           console.log(err);
           res.send('Error occurred');
           return;
         }
+        const profile_image_path = imageResult[0].userImage;
 
-        // 프로필 이미지 blob 데이터를 base64로 디코딩하여 화면에 띄우기
-        const imageBase64 = Buffer.from(imageResult[0].userImage, 'binary').toString('base64');
-        const imageUrl = `data:image/png;base64,${imageBase64}`;
-
-        res.render('mypage', { nickname, bookmarkCount, thumbsCount, profile_image_path: imageUrl });
+        res.render('mypage', { nickname, bookmarkCount, thumbsCount, profile_image_path });
       });
     });
   });
 });
 
-
-router.post('/upload', (req, res) => {
+router.post('/upload', upload.single('profileImage'), (req, res) => {
   const userId = req.session.userIndex;
-  const base64Image = req.body.profileImage;
+  const filePath = req.file.path;
 
-  // base64로 인코딩된 이미지 데이터를 디코딩하여 파일로 저장합니다.
-  const buffer = Buffer.from(base64Image, 'base64');
-  const fileName = `user_${userId}.png`;
-  const filePath = path.join(__dirname, '..', 'public', 'uploads', fileName);
-  fs.writeFile(filePath, buffer, (err) => {
+  // TODO: 데이터베이스에 이미지 경로를 저장합니다.
+  connection.query(`UPDATE users SET userImage = ? WHERE userIndex = ?`, [filePath, userId], (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).send('Error occurred');
       return;
     }
 
-    // 데이터베이스에 이미지 경로를 저장합니다.
-    connection.query(`UPDATE users SET userImage = ? WHERE userIndex = ?`, [`/uploads/${fileName}`, userId], (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error occurred');
-        return;
-      }
-
-      // 이미지를 업로드하고 데이터베이스에 저장했으면 화면에 이미지를 띄웁니다.
-      res.send(`<img src="/uploads/${fileName}" style="width: 100%; height: 100%; border-radius: 100%;">`);
-    });
+    // 이미지를 업로드하고 데이터베이스에 저장했으면 화면에 이미지를 띄웁니다.
+    res.send(`<img src="${filePath}" style="width: 100%; height: 100%; border-radius: 100%;">`);
   });
+});
+
+router.post('/nickname', (req, res) => {
+  const userId = req.session.userIndex;
+  const newNickname = req.body.nickname;
+
+  // 세션에 저장된 유저 닉네임 변경
+  req.session.userNickname = newNickname;
+
+  // 데이터베이스 users 테이블에서 유저 닉네임 변경
+  const sql = `UPDATE users SET usernickname = '${newNickname}' WHERE userIndex = '${userId}'`;
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+  });
+
+  res.send('Nickname changed successfully');
 });
 
 
