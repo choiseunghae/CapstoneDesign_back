a54f621ddcda921ed45d5b3b5c16fd1827fea763
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-05-08 16:38:20 +0900

    signup

--- router/signup.js
+++ router/signup.js
@@ -13,7 +13,7 @@ router.post('/', (req, res) => {
   const usernickname = req.body.usernickname;
   const password = req.body.password;
   const createdAt = new Date(); 
-  const image = NULL;
+  const image = null;
 
   connection.query("INSERT INTO users (usernickname, password, createdAt, userImage) VALUES (?, ?, ?, ?)", [usernickname, password, createdAt, image], (error, result) => {
     if (error) throw error;
