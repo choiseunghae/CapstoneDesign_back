21aead44bd6cb55d5fb988f0cb290f9d1ceab30f
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-05-08 16:52:05 +0900

    heroku

--- mysql.js
+++ mysql.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  
+  /*
   host: '104.154.91.162',
   user: 'root',
   password: '963472',
   database: 'dictionary'
+*/
 
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 
 });
 
