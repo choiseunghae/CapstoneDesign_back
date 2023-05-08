782f593da7ed38c4ea87ebe3db3bcec52669ed47
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-05-08 17:58:54 +0900

    signup

--- router/signup.js
+++ router/signup.js
@@ -14,10 +14,19 @@ router.post('/', (req, res) => {
   const password = req.body.password;
   const createdAt = new Date(); 
   const image = null;
-
-  connection.query("INSERT INTO users (usernickname, password, createdAt, userImage) VALUES (?, ?, ?, ?)", [usernickname, password, createdAt, image], (error, result) => {
+  
+  connection.query("SELECT * FROM users WHERE usernickname = ?", [usernickname], (error, result) => {
     if (error) throw error;
-    res.redirect("/login");
+    if (result.length > 0) {
+      // 이미 존재하는 usernickname인 경우
+      res.redirect("/signup");
+    } else {
+      // 새로운 usernickname인 경우 쿼리 실행
+      connection.query("INSERT INTO users (usernickname, password, createdAt, userImage) VALUES (?, ?, ?, ?)", [usernickname, password, createdAt, image], (error, result) => {
+        if (error) throw error;
+        res.redirect("/login");
+      });
+    }
   });
 });
 
