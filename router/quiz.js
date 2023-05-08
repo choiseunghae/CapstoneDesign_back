b4e1df3b1f8b0078c06506c242cee31aa135f2db
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-05-08 17:53:13 +0900

    heroku

--- router/quiz.js
+++ router/quiz.js
@@ -5,27 +5,27 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.use((req, res,next) => {
-    connection.query(`SELECT MAX(itemIndex) FROM detailpage`, (err, rows) => {
-        if (err) {
-          console.log(err);
-          res.send('Error occurred');
-          return;
-        }
+router.use((req, res, next) => {
+  connection.query(`SELECT MAX(itemIndex) FROM detailpage`, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.send('Error occurred');
+      return;
+    }
 
-        const maxItemIndex = rows[0]['MAX(itemIndex)'];
-        const question = Math.floor(Math.random() * maxItemIndex) + 1;
+    const maxItemIndex = rows[0]['MAX(itemIndex)'];
+    const question = Math.floor(Math.random() * maxItemIndex) + 1;
 
-        connection.query(`SELECT * FROM detailpage WHERE itemIndex = ${question}`, (err, result) => {
-            if (err) {
-              console.log(err);
-              res.send('Error occurred');
-              return;
-            }
+    connection.query(`SELECT * FROM detailpage WHERE itemIndex = ${question}`, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send('Error occurred');
+        return;
+      }
 
-            const correctAnswer = result[0].itemName;
+      const correctAnswer = result[0].itemName;
 
-            const wrongAnswersQuery = `
+      const wrongAnswersQuery = `
               SELECT itemName
               FROM detailpage
               WHERE itemIndex != ${question}
@@ -33,18 +33,18 @@ router.use((req, res,next) => {
               LIMIT 3
             `;
 
-            connection.query(wrongAnswersQuery, (err, wrongAnswers) => {
-                if (err) {
-                  console.log(err);
-                  res.send('Error occurred');
-                  return;
-                }
+      connection.query(wrongAnswersQuery, (err, wrongAnswers) => {
+        if (err) {
+          console.log(err);
+          res.send('Error occurred');
+          return;
+        }
 
-                const options = wrongAnswers.map((item) => item.itemName);
-                options.push(correctAnswer);
-                shuffleArray(options);
+        const options = wrongAnswers.map((item) => item.itemName);
+        options.push(correctAnswer);
+        shuffleArray(options);
 
-                const html = `
+        const html = `
                   <div class="quizbox">
                   <h1>${result[0].itemDescription}</h1>
                   <ul>
@@ -53,23 +53,78 @@ router.use((req, res,next) => {
                   </div>
                 `;
 
-                res.locals.quiz = html;
-                next();
-            });
-        });
+        res.locals.quiz = html;
+        next();
+      });
     });
+  });
 });
 
 
+router.post('/quiz', (req, res) => {
+  connection.query(`SELECT MAX(itemIndex) FROM detailpage`, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.send('Error occurred');
+      return;
+    }
+
+    const maxItemIndex = rows[0]['MAX(itemIndex)'];
+    const question = Math.floor(Math.random() * maxItemIndex) + 1;
+
+    connection.query(`SELECT * FROM detailpage WHERE itemIndex = ${question}`, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send('Error occurred');
+        return;
+      }
+
+      const correctAnswer = result[0].itemName;
+
+      const wrongAnswersQuery = `
+          SELECT itemName
+          FROM detailpage
+          WHERE itemIndex != ${question}
+          ORDER BY RAND()
+          LIMIT 3
+        `;
+
+      connection.query(wrongAnswersQuery, (err, wrongAnswers) => {
+        if (err) {
+          console.log(err);
+          res.send('Error occurred');
+          return;
+        }
+
+        const options = wrongAnswers.map((item) => item.itemName);
+        options.push(correctAnswer);
+        shuffleArray(options);
+        const html = `
+    <div class="quizbox">
+      <h1>${result[0].itemDescription}</h1>
+      <ul>
+        ${options.map((option) => `<li button class="gradient-btn" onclick="checkAnswer('${option}', '${correctAnswer}')">${option}</li>`).join('')}
+      </ul>
+    </div>
+  `;
+
+        // HTML 코드를 클라이언트에게 응답
+        res.send(html);
+      });
+    });
+  });
+});
+
 // Shuffle array in place
 function shuffleArray(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
 }
 
 module.exports = router;
+
 
 /*
 <script>
