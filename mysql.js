123eecf29d4f630d9fc536f373ab09746b933e0f
byeonjieun <bje6365@gmail.com>
2023-05-16 16:09:49 +0900

    jieun

--- mysql.js
+++ mysql.js
@@ -1,23 +1,17 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-<<<<<<< HEAD
-=======
-  
->>>>>>> 8cf29c1f2a85a147c5023f431f7e8fcb66353d4d
-  host: '104.154.91.162',
-  user: 'root',
-  password: '963472',
-  database: 'dictionary'
-<<<<<<< HEAD
-=======
+
+host: '104.154.91.162',
+user: 'root',
+password: '963472',
+database: 'dictionary'
 
   // host: process.env.DB_HOST,
   // user: process.env.DB_USER,
   // password: process.env.DB_PASSWORD,
   // database: process.env.DB_NAME
 
->>>>>>> 8cf29c1f2a85a147c5023f431f7e8fcb66353d4d
 });
 
 connection.connect();
