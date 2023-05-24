6e46b380d49a507acdd30522f0f452d6bc1529a4
choiseunghae <118813577+choiseunghae@users.noreply.github.com>
2023-05-24 16:19:30 +0900

    mysql

--- mysql.js
+++ mysql.js
@@ -1,23 +1,16 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-<<<<<<< HEAD
   /*
-=======
-  
->>>>>>> 1acca4ae9793bb7b7743cca28b6743ef520b60a5
   host: '104.154.91.162',
   user: 'root',
   password: '963472',
   database: 'dictionary'
-*/
-
-  /*
+  */
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-*/
 });
 
 connection.connect();
