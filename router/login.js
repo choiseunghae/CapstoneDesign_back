61d5c8def60518a67989fc8141cfcdfb2a539f3d
(yeojin youn) <indiayeojin@gmail.com>
2023-05-30 15:58:52 +0900

    test

--- router/login.js
+++ router/login.js
@@ -18,6 +18,11 @@ router.post('/', (req, res) => {
     const usernickname = req.body.usernickname;
     const password = req.body.password;
 
+    // 닉네임과 비밀번호가 입력되었는지 확인합니다.
+    if (!usernickname || !password) {
+        return res.json({ success: false, message: '닉네임과 비밀번호를 입력해주세요.' });
+    }
+
     connection.query("SELECT * FROM users WHERE usernickname = ? AND password = ?", [usernickname, password], (error, result) => {
         if (error) throw error;
         if (result.length === 1) {
@@ -40,9 +45,9 @@ router.post('/', (req, res) => {
             res.cookie('authToken', authToken, { maxAge: 86400000, httpOnly: true }); // 쿠키에 인증 토큰 저장
             req.session.userIndex = userIndex; // 세션에 userIndex 저장
             req.session.usernickname = usernickname; // 세션에 usernickname 저장
-            res.redirect("/");
+            res.json({ success: true, redirectUrl: "/" });
         } else {
-            res.render('login', { message: '회원 정보가 다릅니다.' });
+            res.json({ success: false, message: '회원 정보가 다릅니다.' });
         }
     });
 });
